048617712e8b5dd3d90ea33e1558960f028268c8
Adam Cox <adam.cox@vorba.com>
2019-09-05 21:16:03 -0400

    after generate.bat

---
+++ api/api.ts
@@ -0,0 +1,5 @@
+export * from './tblPriceBookMains.service';
+import { TblPriceBookMainsService } from './tblPriceBookMains.service';
+export * from './values.service';
+import { ValuesService } from './values.service';
+export const APIS = [TblPriceBookMainsService, ValuesService];
